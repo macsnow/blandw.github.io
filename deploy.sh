123176b56fe2d58292b9ad382407e9d4d3570206
macsnow <cuitwubo@gmail.com>
2022-10-09 16:42:04 +0800

    update repo name

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:macsnow/vueprocess-starter.git master:gh-pages
+git push -f git@github.com:macsnow/vuepress-starter.git master:gh-pages
 
 cd -
